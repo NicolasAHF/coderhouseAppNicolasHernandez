6381c8f016e056d47ed365e0d380cd28d2bb2479
NicolasAHF <nicolashernandezf22@gmail.com>
2023-01-18 14:41:10 -0300

    PreEntrega2-Hernandez

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
-import './App.css';
-import ItemListContainer from './components/Items/ItemListContainer';
-import CarouselComponent from './components/Navigation/Carousel';
-import NavBar from './components/Navigation/NavBar';
-import './custom.scss';
+import "./App.css";
+import ItemListContainer from "./components/Items/ItemListContainer";
+import CarouselComponent from "./components/Navigation/Carousel";
+import NavBar from "./components/Navigation/NavBar";
+import "./custom.scss";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ItemDetail from "./components/Items/ItemDetail";
 
 function App() {
-  return (<BrowserRouter>
-    <NavBar/>
-    <CarouselComponent />
-    <Routes>
-      <Route path="/" element={<ItemListContainer />}/>
-      <Route path="category/:id" element={<ItemListContainer />}/>
-      <Route path="item/:id" element={<ItemListContainer />}/>
-    </Routes>
-  </BrowserRouter>
+  return (
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <CarouselComponent />
+              <ItemListContainer />
+            </>
+          }
+        />
+        <Route path="category/:id" element={<ItemListContainer />} />
+        <Route path="item/:id" element={<ItemDetail />} />
+        <Route path="*" element={<h1 style={{textAlign:"center"}}>404 La ruta no existe</h1>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
